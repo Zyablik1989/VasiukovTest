0b01f8c9d30b22fd8677580222612f34f1b49673
alex <vasyukovAlexey@yandex.ru>
2022-10-22 17:30:37 +0500

    2_AUTOTEST added basic date manipulations. Also the commentary for the dates problem has been added

---
+++ 2_AUTOTEST/FebruaryTask/Fabruary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabruary
+{
+    class Program
+    {
+        static readonly DateTime TaskFabruaryDate = new DateTime(2020, 2, 29);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Изначальная дата из задачи");
+            DisplayDate(TaskFabruaryDate);
+
+            Console.WriteLine("Изначальная дата из задачи, конец месяца");
+            var TaskFabruaryEndDate = GetEndingOfGivenMonth(TaskFabruaryDate);
+            DisplayDate(TaskFabruaryEndDate);
+
+            var CurrentYearFabruaryStart = new DateTime(year: DateTime.Now.Year, month: 2, day: 1);
+            Console.WriteLine("Начало февраля текущего года");
+            DisplayDate(CurrentYearFabruaryStart);
+
+            var CurrentYearFabruaryEnd = GetEndingOfGivenMonth(CurrentYearFabruaryStart);
+            Console.WriteLine("Конец февраля текущего года");
+            DisplayDate(CurrentYearFabruaryEnd);
+
+
+            Console.ReadLine();
+        }
+
+        static DateTime GetEndingOfGivenMonth(DateTime date)
+        {
+            return
+                new DateTime(year: date.Year, month: date.Month, day: 1)
+                .AddMonths(1)
+                .AddMilliseconds(-1);
+        }
+
+        static void DisplayDate(DateTime date)
+        {
+            Console.WriteLine(date.ToString("G"));
+        }
+    }
+}
